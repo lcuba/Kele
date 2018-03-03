62dcd39687ed1c79554d15921b45c5529bed5f91
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-03-03 19:06:26 +0000

    Retrieve Users checkpoint work complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,20 +1,27 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
+    include JSON
    
    
    def initialize(email, password)
        user_info = {body: {email: email, password: password}}
-       response = self.class.post(api_url("sessions"), user_info)
+       response = self.class.post(url("sessions"), user_info)
        
        raise "Invalid email or password" unless response.code == 200
        @auth_token = response["auth_token"]
    end
    
+   def get_me
+       response = self.class.get(url("users/me"), headers: {"authorization" => @auth_token})
+       @user = JSON.parse(response.body)
+   end
+   
    private
   
-   def api_url(endpoint)
+   def url(endpoint)
       "https://www.bloc.io/api/v1/#{endpoint}" 
    end
 end
