2ecc800577706cd6070af97a985c077ac4b0f3de
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-03-05 05:53:36 +0000

    Roadmaps and Checkpoints checkpoint work completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'json'
+require 'lib/roadmap.rb'
 
 class Kele
     include HTTParty
     include JSON
+    include Roadmap
    
    
    def initialize(email, password)
